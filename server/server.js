81e43b45f2ea12abe5c2fabcfbd848a5e3d74ff0
Travin Grant <travingrant@gmail.com>
2019-03-09 04:48:25 +0000

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
+  var todo = new Todo({
     text: req.body.text
   });
 
@@ -23,8 +21,8 @@ app.post('/todos', (req, res) => {
   });
 });
 
-
-
 app.listen(3000, () => {
-    console.log('Started on port 3000');
+  console.log('Started on port 3000');
 });
+
+module.exports = {app};
